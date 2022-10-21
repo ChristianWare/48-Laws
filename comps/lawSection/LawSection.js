ab22fcf27dcdf6086fda99faf1221cfce20274be
Chris Ware <c.p.ware@hotmail.com>
2022-10-20 23:36:14 -0700

    Law Section

---
+++ comps/lawSection/LawSection.js
@@ -0,0 +1,12 @@
+import styles from "./LawSection.module.css";
+
+const LawSection = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        Individual Law Components goe here...
+      </div>
+    </section>
+  );
+};
+export default LawSection;
