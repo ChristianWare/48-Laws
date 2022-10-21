b8d7cbe1a2c288ceab7eefa3e0df3e075162aeda
Chris Ware <c.p.ware@hotmail.com>
2022-10-21 14:37:49 -0700

    law Files Created + NPM Dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import Link from "next/link";
 import Hero from "../comps/hero/Hero";
 import LawSection from "../comps/lawSection/LawSection";
 
-export default function Home() {
+export default function Home({ laws }) {
+  console.log(laws)
   return (
     <div>
       <Head>
@@ -13,6 +15,40 @@ export default function Home() {
 
       <Hero />
       <LawSection />
+      {laws.map((law, index) => (
+        <Link href={`/laws/${law.slug}`} key={index}>
+          <a>{law.frontmatter.title}</a>
+        </Link>
+      ))}
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const fs = require("fs");
+  const path = require("path");
+  const matter = require("gray-matter");
+
+  //1. Find the path in the directory called "laws":
+  const files = fs.readdirSync(path.join("laws"));
+
+  //2. Gain access to all og the files inside of the "laws" directory:
+  const laws = files.map((filename) => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join("laws", filename),
+      "utf-8"
+    );
+
+    //3. Extract the front matter from every MDX file:
+    const { data } = matter(markdownWithMeta);
+
+    return {
+      frontmatter: data,
+      slug: filename.split(".")[0],
+    };
+  });
+
+  return {
+    props: { laws },
+  };
+};
